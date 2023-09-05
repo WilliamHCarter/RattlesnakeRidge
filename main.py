0f38a3f62c1943d86851c3d2e5ed4b06e765119e
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-09-05 15:39:09 -0400

    Added Clues, cleaned main

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI, FakeListChatModel
 import os
-
+import yaml
 from agents import Agent, AgentConversation
 
-import yaml
+#===== Setup the env and chat model =====#
 with open('data/prompts.yaml', 'r') as file:
     raw = file.read()
 prompts = yaml.safe_load(raw)
-
 load_dotenv()
+
 #For local builds, go to .env.template file and follow directions.
 api_key = os.environ.get('LLM_API_KEY')
-
 model = 'gpt-3.5-turbo'
 # llm = ChatOpenAI(openai_api_key=api_key, model=model)
 llm = FakeListChatModel(verbose=True, responses=['Howdy, stranger. What brings you to these parts today?'])
 
+
+#===== Test Conversation with Flint =====#
 flint_agent = Agent(datafile='data/flint.yaml')
 conversation = AgentConversation(flint_agent, prompts['single_person_conversation'], llm)
-
 # user_message = input("What would you like to say? ")
 user_message = "Hello there!"
 print(f'> {user_message}')
