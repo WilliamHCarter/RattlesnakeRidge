4e879ebb0a856773185c7d1c82338a7e56d87b73
Aidan McHugh <mchughaw@gmail.com>
2023-09-25 23:46:15 -0400

    Formatting and todo

--- scenes.py
+++ scenes.py
@@ -9,7 +9,7 @@ def get_user_input(input_message: str, valid_inputs: list[int]):
         try:
             number = int(user_input)
             if number in valid_inputs:
-                return number -1
+                return number
             else:
                 print("Number is out of range!")
                 
@@ -22,14 +22,14 @@ def first_day_intro(agents: list[Agent], player: PlayerAgent, llm_data: LLMData)
     remaining_intro = copy(agents)
     print(
         """
-        \n As the sun sets on the horizon, you ride into the dusty outpost 
-        of Rattlesnake Ridge. The villagers are gathered around the town 
-        center, murmuring about a heinous crime: a local prospector named Jeb, 
-        known for recently striking gold, has been found dead. Word is that 
-        his stash of gold is missing too. You decide to step in, and after 
-        introducing yourself, you have the option to speak to the main 
-        suspects: Whistle, Miss Clara, Marshal Flint, and Billy "Snake Eyes" 
-        Thompson. \n
+\n As the sun sets on the horizon, you ride into the dusty outpost 
+of Rattlesnake Ridge. The villagers are gathered around the town 
+center, murmuring about a heinous crime: a local prospector named Jeb, 
+known for recently striking gold, has been found dead. Word is that 
+his stash of gold is missing too. You decide to step in, and after 
+introducing yourself, you have the option to speak to the main 
+suspects: Whistle, Miss Clara, Marshal Flint, and Billy "Snake Eyes" 
+Thompson. \n
         """
     )
     for _ in range(4):
@@ -39,8 +39,8 @@ def first_day_intro(agents: list[Agent], player: PlayerAgent, llm_data: LLMData)
             for i, agent in enumerate(remaining_intro):
                 print(f"{str(i+1)}: {agent.name} -- {agent.short_description}")
             input_range = range(1,len(remaining_intro)+1)
-            in_message = "Enter a number (1-"+str(len(input_range))+"):"
-            selection = get_user_input(in_message, input_range)
+            in_message = "Enter a number (1-"+str(len(input_range))+"): "
+            selection = get_user_input(in_message, input_range) - 1
             selected_agent = remaining_intro[selection]
         else:
             selected_agent = remaining_intro[0]
@@ -82,10 +82,10 @@ def first_day_intro(agents: list[Agent], player: PlayerAgent, llm_data: LLMData)
 def first_night_cutscene():
     print(
         """
-        The moon is high when a piercing scream echoes through the night. 
-        Everyone rushes out to find Whistle's Saloon in disarray – a scuffle 
-        has occurred. You notice a bloodied poker card on the floor, the ace 
-        of spades. This might be a clue, but to what? \n
+The moon is high when a piercing scream echoes through the night. 
+Everyone rushes out to find Whistle's Saloon in disarray -- a scuffle 
+has occurred. You notice a bloodied poker card on the floor, the ace 
+of spades. This might be a clue, but to what?
         """
     )
 
@@ -93,13 +93,13 @@ def first_night_cutscene():
 def second_day_intro(agents: list[Agent], player: PlayerAgent, llm_data: LLMData):
     print(
         """ 
-        Sunlight reveals tense faces. The townfolk have formed two groups.
-        On one side, by the water trough, stands Whistle, looking ruffled, 
-        and Miss Clara, her comforting hand on his arm. They seem to be
-        arguing with the other group, consisting of Marshal Flint and
-        Billy, who are on the steps of the Marshal's Office. You need
-        to make a choice quickly: which duo will you approach to get
-        their side of the story?\n
+Sunlight reveals tense faces. The townfolk have formed two groups.
+On one side, by the water trough, stands Whistle, looking ruffled, 
+and Miss Clara, her comforting hand on his arm. They seem to be
+arguing with the other group, consisting of Marshal Flint and
+Billy, who are on the steps of the Marshal's Office. You need
+to make a choice quickly: which duo will you approach to get
+their side of the story?
         """
     )
 
@@ -116,8 +116,8 @@ def second_day_intro(agents: list[Agent], player: PlayerAgent, llm_data: LLMData
     ]
     
     input_range = range(1,len(b_and_c)+1)
-    in_message = "Enter a number (1-"+str(len(input_range))+"):"
-    selection = get_user_input(in_message, input_range)    
+    in_message = "Enter a number (1-"+str(len(input_range))+"): "
+    selection = get_user_input(in_message, input_range) - 1
     agent_order: list[Agent] = b_and_c if selection == 0 else f_and_w
 
     # Another time-bound convo
@@ -141,9 +141,9 @@ def second_day_intro(agents: list[Agent], player: PlayerAgent, llm_data: LLMData
         responses_left -= 1
 
     print(
-        """\n
-        A sudden gunshot rings out, interrupting your conversation. The 
-        townsfolk scatter, heading to their homes or businesses to seek cover.
+        """
+A sudden gunshot rings out, interrupting your conversation. The 
+townsfolk scatter, heading to their homes or businesses to seek cover.
         """
     )
 
@@ -151,8 +151,8 @@ def second_day_intro(agents: list[Agent], player: PlayerAgent, llm_data: LLMData
 def second_day_afternoon(agents: list[Agent], player: PlayerAgent, llm_data: LLMData):
     print(
         """
-        The town is quieter now, and the townspeoples' nerves are on edge. 
-        You have the chance to speak to one more person in-depth. \n
+The town is quieter now, and the townspeoples' nerves are on edge. 
+You have the chance to speak to one more person in-depth.
         """
     )
 
@@ -162,8 +162,8 @@ def second_day_afternoon(agents: list[Agent], player: PlayerAgent, llm_data: LLM
         print(f"{str(i+1)}: {agent.name} -- {agent.short_description}")
     
     input_range = range(1,len(agents)+1)
-    in_message = "Enter a number (1-"+str(len(input_range))+"):"
-    selection = get_user_input(in_message, input_range)    
+    in_message = "Enter a number (1-"+str(len(input_range))+"): "
+    selection = get_user_input(in_message, input_range) - 1
 
     selected_agent = agents[selection]
     print(f"\nTime to talk to {selected_agent.name}\n")
@@ -189,11 +189,11 @@ def second_day_afternoon(agents: list[Agent], player: PlayerAgent, llm_data: LLM
 def final_confrontation(agents: list[Agent], player: PlayerAgent, llm_data: LLMData):
     print(
         """
-        Night has fallen. You gather everyone in the Saloon, where the mood is 
-        palpable. Shadows dance on the walls as you stand before the suspects. 
-        Here, you must make your case to the townfolk, after which you must 
-        aim your gun and pulling the trigger on the character you believe to 
-        be the killer.\n
+Night has fallen. You gather everyone in the Saloon, where the mood is 
+palpable. Shadows dance on the walls as you stand before the suspects. 
+Here, you must make your case to the townfolk, after which you must 
+aim your gun and pulling the trigger on the character you believe to 
+be the killer.
         """
     )
 
@@ -208,6 +208,9 @@ def final_confrontation(agents: list[Agent], player: PlayerAgent, llm_data: LLMD
         for i, r in enumerate(responses):
             if r.text:
                 print(f"{r.agent}: {r.text}")
+            # Todo:: for the final confrontation, we should not let the agents make the 
+            # conversation end early. Instead, they should be allowed to 'exit' the 
+            # conversation by refusing to say anything more.
             if r.conversation_ends:
                 responses_left = 0
 
@@ -221,30 +224,29 @@ def final_confrontation(agents: list[Agent], player: PlayerAgent, llm_data: LLMD
     print()
     selected_agent = agents[selection]
 
+    # todo:: Make the character say dying words to the player
     if selected_agent.name == "Whistle":
         print(
             """
-        You aim your gun at Whistle, and pull the trigger. The bullet
-        flies through the air, and hits Whistle square in the chest. He
-        falls to the ground, dead. The townsfolk cheer, and you are
-        hailed as a hero. The ghost of Jeb can rest easy.\n
+You aim your gun at Whistle, and pull the trigger. The bullet
+flies through the air, and hits Whistle square in the chest. He
+falls to the ground, dead. The townsfolk cheer, and you are
+hailed as a hero. The ghost of Jeb can rest easy.\n
         """
         )
     else:
         print(
-            """
-            As""",
-            selected_agent.name,
-            """crumples to the ground, chaos ensues. 
-            The real killer takes advantage of the confusion, locking you up 
-            in the Marshal's Office with accusations of murder, while they 
-            make their escape, leaving you with the weight of your misjudgment.
+            f"""
+As {selected_agent.name} crumples to the ground, chaos ensues. 
+The real killer takes advantage of the confusion, locking you up 
+in the Marshal's Office with accusations of murder, while they 
+make their escape, leaving you with the weight of your misjudgment.
             """,
         )
 
     print(
         """
-        Thanks For Playing Rattlesnake Ridge!
-        Made by Will Carter and Aidan McHugh
+    Thanks For Playing Rattlesnake Ridge!
+    Made by Will Carter and Aidan McHugh
         """
     )
