dc87d293fea613d9bd82f37e3a38dc2306781fa6
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-09-29 17:23:49 -0400

    CRT V0.1

--- client/src/components/CrtScreen.tsx
+++ client/src/components/CrtScreen.tsx
@@ -1,6 +1,26 @@
 function CrtScreen() {
   return (
-    <div className="flex self-center w-[90vw] h-[65vh] rounded-xl bg-[#161e28]"></div>
+    <div className="flex self-center w-[85vw] h-[65vh] rounded-xl relative">
+      <div className="rounded-xl absolute h-full w-full bg-gradient-radial from-[#042625] to-[#0c1919]" />
+      <div
+        className="CRT Filter rounded-xl"
+        style={{
+          pointerEvents: "none",
+          overflow: "hidden",
+          position: "absolute",
+          zIndex: 11,
+          width: "100%",
+          height: "100%",
+          opacity: "60%",
+          background:
+            "linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06))",
+          backgroundSize: "100% 3px, 6px 100%",
+        }}
+      />
+      <h1 className="text-lg text-green-600 font-mono p-4 z-10">
+        Welcome to Rattlesnake Ridge...
+      </h1>
+    </div>
   );
 }
 
