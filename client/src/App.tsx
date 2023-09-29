7ddc05f89250f9cb1c10796d0a9a363cbc74d869
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-09-29 18:03:22 -0400

    Add Divider, package-lock

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="mx-auto flex flex-col">
       <Navbar />
+      <div className="Divider shrink-0 bg-border h-[1px] w-full" />
       <div className="py-4"/>
       <CrtScreen/>
     </div>
