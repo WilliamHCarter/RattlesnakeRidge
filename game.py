14d0c3f9de8d80915ab18f67c348c64e187ac1bf
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-10-09 03:47:06 -0400

    Game State Refactor Draft

--- game.py
+++ game.py
@@ -1,25 +1,85 @@
-from scenes import first_day_intro, first_night_cutscene, second_day_intro, second_day_afternoon, final_confrontation
+import os
+import yaml
+from dotenv import load_dotenv
+from dataclasses import dataclass
+from agents.conversation import LLMData
+from agents.agent import Agent, PlayerAgent
+from langchain.chat_models import ChatOpenAI, FakeListChatModel
+from flask import session
+from scenes import (
+    first_day_intro,
+    first_night_cutscene,
+    second_day_intro,
+    second_day_afternoon,
+    final_confrontation,
+)
 
 # A mapping of scene names to scene functions
 SCENES = {
-    'first_day_intro': first_day_intro,
-    'first_night_cutscene': first_night_cutscene,
-    'second_day_intro': second_day_intro,
-    'second_day_afternoon': second_day_afternoon,
-    'final_confrontation': final_confrontation,
+    "first_day_intro": first_day_intro,
+    "first_night_cutscene": first_night_cutscene,
+    "second_day_intro": second_day_intro,
+    "second_day_afternoon": second_day_afternoon,
+    "final_confrontation": final_confrontation,
 }
 
-def play(game_state, user_input):
+
+@dataclass
+class GameState:
+    agents: list[Agent | PlayerAgent]
+    player: PlayerAgent
+    current_scene: str
+    llm_data: LLMData
+
+
+def initialize_game() -> GameState:
+    def load_dict(filename: str) -> dict:
+        with open(filename, "r") as file:
+            raw = file.read()
+        return yaml.safe_load(raw)
+
+    # ===== Setup the env and chat model =====#
+    prompts = load_dict("data/prompts.yaml")
+    setting = load_dict("data/setting.yaml")
+    load_dotenv()
+
+    # For local builds, go to .env.template file and follow directions.
+    api_key = os.environ.get("LLM_API_KEY")
+    model = "gpt-3.5-turbo"
+
+    # Set the Model
+    llm = FakeListChatModel(
+        verbose=True,
+        responses=[
+            f"Hi there, I'm talking to you.",
+            "That is not nice",
+            "[QUIT] This conversation is over.",
+        ],
+    )
+    # llm = ChatOpenAI(openai_api_key=api_key, model=model)
+    llm_data: LLMData = LLMData(
+        llm, prompts["single_person_conversation_complex"], setting
+    )
+
+    # ===== Setup the agents =====#
+    character_names = ["flint", "billy", "clara", "whistle"]
+
+    agents = [
+        Agent(datafile=f"data/characters/{name}.yaml") for name in character_names
+    ]
+    player = PlayerAgent(datafile="data/characters/player.yaml")
+
+    return GameState(
+        agents=agents,
+        player=player,
+        current_scene="first_day_intro",
+        llm_data=llm_data,
+    )
+
+
+def play(gs: GameState, user_input: str):
     # Get the current scene from the game state
-    current_scene = game_state.get('current_scene', 'first_day_intro')
-
-    # Get the scene function from the SCENES mapping
-    scene_function = SCENES.get(current_scene)
-
-    if scene_function:
-        # Call the scene function and pass the current game state and user input
-        response, new_game_state = scene_function(game_state, user_input)
-        return response, new_game_state
+    response = gs.current_scene(gs.agents, gs.player, gs.llm_data, user_input)
 
     # Handle the case where the scene is not found
     return "System Error, scene not found", None
