72202377b9f849d496c1777be74000b487a793a6
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-10-13 17:19:27 -0400

    Mobile Menu

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import MobileMenu from "./MobileMenu";
 import SettingsButton from "./SettingsButton";
 import SourceButton from "./SourceButton";
 import ThemeButton from "./ThemeButton";
@@ -5,12 +6,16 @@ import ThemeButton from "./ThemeButton";
 function Navbar() {
   return (
     <div className="flex w-[100vw] justify-between px-4 py-4">
-      <h1 className="text-3xl font-semibold">Rattlesnake Ridge</h1>
+      <h1 className="tablet:text-3xl text-2xl self-center font-semibold">Rattlesnake Ridge</h1>
       <nav className="flex gap-6 items-center">
-        <div className="flex gap-4">
+        <div className="tablet:flex hidden gap-4">
           <SourceButton />
           <ThemeButton />
           <SettingsButton />
+        </div>
+        <div className="flex tablet:hidden gap-4">
+          <ThemeButton />
+          <MobileMenu />
         </div>
       </nav>
     </div>
