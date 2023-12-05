805e224be199391f74b4c5d29bde7d272664eab1
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-12-05 10:11:27 -0500

    Remove Debug Comments

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,6 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
 import {
-  BaseCommand,
   SelectOptionCommand,
   castCommand,
   extractTextContent,
@@ -23,20 +22,16 @@ const fetchGame = async (url: string, gameID: string, userIn: string = "") => {
 };
 
 export const loadGame = async (props: SGProps) => {
-  console.log("[LOADING GAME]", localStorage.getItem("game_id"));
   let gameID = localStorage.getItem("game_id") as string;
   props.setGameID(gameID);
   const loadedGame = await fetchGame("http://127.0.0.1:5000/load/", gameID);
 
   if (!loadedGame.ok) {
-    console.log("[RESPONSE NOT OK, RESTARTING GAME]");
     localStorage.removeItem("game_id");
     return false;
   }
 
   const data = await loadedGame.json();
-  console.log("[RESPONSE OK, SETTING CONVERSATION As]", data);
-
   data.response.forEach((response: any) => {
     var { uText, uStyles } = processResponse(response);
     uStyles = uStyles.map((style) => ({ ...style, characterDelayMs: 0 }));
@@ -48,19 +43,15 @@ export const loadGame = async (props: SGProps) => {
 };
 
 export const startGame = async (props: SGProps) => {
-  console.log("[STARTING GAME]");
   const response = await fetch("http://127.0.0.1:5000/start");
 
   if (!response.ok) {
-    console.log("[RESPONSE NOT OK]");
     return;
   }
-  console.log("[RESPONSE OK]");
   const data = await response.json();
   console.debug(data);
   if (data.message && typeof data.message === "string") {
     if (!data.styles) data.styles = new TextStyles("game start");
-    console.log("[SETTING GAME_ID AND CONVERSATION]");
     props.handleConversation([data.message], [data.styles]);
     props.setGameID(data.game_id);
     localStorage.setItem("game_id", data.game_id);
@@ -78,7 +69,6 @@ export async function ply(
   userInput: string,
   handleConversation: Function
 ) {
-  console.log("[PLYING]: ", userInput);
   const response = await fetchGame(
     "http://127.0.0.1:5000/play/",
     gameID,
