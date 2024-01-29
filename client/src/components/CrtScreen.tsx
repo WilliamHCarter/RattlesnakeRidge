97b0327109520b18e1092192fa48bc7b86b54308
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2024-01-28 23:51:10 -0500

    Import bug fix

--- client/src/components/CrtScreen.tsx
+++ client/src/components/CrtScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AsciiBanner from "./AsciiBanner";
 import Typewriter, { TextStyles } from "./Typewriter";
 import "../index.css";
