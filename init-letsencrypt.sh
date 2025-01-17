fd1600030d6822a62718f29887c375762d8a9b6e
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2025-01-16 23:47:56 -0500

    Clean slate for new deployment

--- init-letsencrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run Certbot to obtain or renew the SSL/TLS certificate
-certbot --nginx -d api.stories.williamcarter.dev
-
-# Reload Nginx to apply the new certificate
-nginx -s reload
